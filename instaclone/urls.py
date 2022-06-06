bf0ebb9b8ffb342d64db72d56a3efa9dd057321a
halima precious <halimaprecious3@gmail.com>
2022-06-06 21:00:37 +0300

    feat:register models

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path('admin/', admin.site.urls),
-    re_path('',include('insta.urls'))
+    re_path('',include('insta.urls')),
+    
+    re_path('accounts/', include('django_registration.backends.activation.urls')),
+    re_path('accounts/', include('django.contrib.auth.urls')),
+    re_path('logout/', auth_views.logout_then_login), 
 ]
