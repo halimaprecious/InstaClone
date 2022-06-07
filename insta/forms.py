6e508d4e9495fb305a43154133e65bfcd408311a
halima precious <halimaprecious3@gmail.com>
2022-06-07 19:36:51 +0300

    feat:post details

--- insta/forms.py
+++ insta/forms.py
@@ -4,21 +4,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'prompt srch_explore'}), max_length=50, required=True)
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'prompt srch_explore'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'prompt srch_explore'}))
-
-    class Meta:
-        model = User
-        widgets = {
-            'username' : forms.TextInput(attrs = {'placeholder': 'Username'}),
-            'email'    : forms.TextInput(attrs = {'placeholder': 'E-Mail'}),
-            'password'    : forms.TextInput(attrs = {'placeholder': 'password'}),
-        }
-        fields = ['username', 'email', 'password1', 'password2']
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude =['poster','image']
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude =['user','profile','likes']
+
